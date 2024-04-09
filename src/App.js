74f8549594f872910ef0054701e4e82c1be17a3b
salih avcı <casus_salih_16@hotmail.com>
2024-04-09 21:25:11 +0300

    Counter ile gün sayma uygulaması yazıldı. Setstate örneği iiçin uygulama tamamlandı.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function handleStepUp() {
+    setStep(step + 1);
+  }
+
+  function handleStepDown() {
+    setStep(step - 1);
+  }
+
+  function handleCountUp() {
+    setCount(count + step);
+  }
+
+  function handleCountDown() {
+    setCount(count - step);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <button onClick={handleStepDown}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={handleStepUp}>+</button>
+      </div>
+      <div>
+        <button onClick={handleCountDown}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={handleCountUp}>+</button>
+      </div>
+      <div>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}{" "}
+          {date.toLocaleDateString()}
+        </span>
+      </div>
     </div>
   );
 }
-
 export default App;
